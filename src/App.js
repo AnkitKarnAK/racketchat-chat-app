5fb2f8762fa5b065c5b256bb5fb04e70638da822
Ankit Karn <ankit.karn.ak@gmail.com>
2021-09-09 04:20:04 +0530

    fix / route to messenger

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
-import Profile from "./components/profile/Profile";
 import Messenger from "./components/messenger/Messenger";
 import { PrivateRoute } from "./components/PrivateRoute";
 
@@ -10,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Profile />} />
+        <PrivateRoute path="/" element={<Messenger />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <PrivateRoute path="/messenger" element={<Messenger />} />
